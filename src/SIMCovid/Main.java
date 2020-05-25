ccb1860a0b10e1655bc195a4c966b39e852787c6
ArientiIvan <i.arienti@studenti.gobettivolta.gov.it>
2020-05-25 20:50:16 +0200

    commit

--- src/SIMCovid/Main.java
+++ src/SIMCovid/Main.java
@@ -1,32 +1,10 @@
 package SIMCovid;
 
-import java.util.LinkedList;
-
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		LinkedList<Umano> u = new LinkedList<Umano>();
-		Page p = new Page(u);
-		for (int i=0; i<5 ; i++) {
-			Umano m = new Umano(1);
-			u.add(m);
-		}
-		for (int i=5; i<10 ; i++) {
-			Umano m = new Umano(0);
-			u.add(m);
-		}
-		p.disegna();
-		Thread.sleep(2000);
-		for (int i=5; i<10 ; i++) {
-			u.get(i).setSalute(1);
-		}
-		p.disegna();
-		Thread.sleep(2000);
-		for (int i=5; i<10 ; i++) {
-			u.get(i).setSalute(0);
-		}
-		p.disegna();
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
