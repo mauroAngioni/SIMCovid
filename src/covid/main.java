c8dee5eb25609cc9cf25c6d7fba3d117ab571c27
Alessio-ctrl <a.petrucci@studenti.gobettivolta.gov.it>
2020-05-25 19:36:59 +0200

    prima versione

---
+++ src/covid/main.java
@@ -0,0 +1,15 @@
+package covid;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   JFrame f = new JFrame();
+	        f.setSize(500, 300);
+	        f.getContentPane().add(new SampleComponent());
+	        f.setVisible(true);
+	}
+
+}
