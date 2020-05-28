a372c326313e4ef5f8313c97cbc4e89cc09e03b1
GiorgiMarco <62608608+GiorgiMarco@users.noreply.github.com>
2020-05-28 10:34:27 +0200

    Main con variabili
    aggiunto refresh muovi e controllocontagio

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,27 @@ import java.util.LinkedList;
 class Main {
 
   static public int coeffdiffusione;
-  static public int infettività;
+  static public int range;
   static public int resistenzaAlVirus;
   static public int durataVirus;
+  static public double infettività;  
+  static public double imp_difese;
+  static public double imp_distanza;
 
-
+  Main m = new Main();
+  m.controlloContagio();
+  m.muovi();
+  m.refresh();
 
   public static void main(String[] args){
+
     Scanner input = new Scanner(System.in);
     System.out.println("Coefficiente di diffusione: ");
     coeffdiffusione = input.nextInt();
 
     Scanner input2 = new Scanner(System.in);
-    System.out.println("Infettività: ");
-    infettività = input2.nextInt();
+    System.out.println("Range: ");
+    range = input2.nextInt();
 
     Scanner input3 = new Scanner(System.in);
     System.out.println("Resistenza al virus: ");
@@ -26,6 +33,21 @@ class Main {
     Scanner input4 = new Scanner(System.in);
     System.out.println("Durata virus: ");
     durataVirus = input4.nextInt();
+
+    Scanner input5 = new Scanner(System.in);
+    System.out.println("Infettività: ");
+    infettività = input5.nextInt();
+
+    Scanner input6 = new Scanner(System.in);
+    System.out.println("Importanza difese: ");
+    imp_difese = input5.nextInt();
+
+    Scanner input7 = new Scanner(System.in);
+    System.out.println("Importanza distanza: ");
+    imp_distanza = input5.nextInt();
+ 
+
   }//METODO MAIN
 
 }//CLASSE MAIN
+
